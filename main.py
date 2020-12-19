e928a35df656412fd70fc0dcbec3a74d986b0c82
DESKTOP-HJLQUDI\EasyK <tomasz.milewski.194@gmail.com>
2020-12-19 09:30:52 +0100

    - few fixes

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class MyWindow(QWidget):
     def initUI(self):
         self.setGeometry(0, 0, 600, 600)
         self.setFixedSize(600, 600)
-        self.resize(100, 100)
 
         self.layout = QVBoxLayout(self)
         self.scrollArea = QScrollArea(self)
@@ -38,24 +37,8 @@ class MyWindow(QWidget):
         self.products = self.reader.readProducts()
         for item in self.products:
             print(item)
-            index = self.products.index(item) + 1
-            idValueLabel = QLabel(str(item['id']), self)
-            nameValueLabel = QLabel(str(item['name']), self)
-            priceValueLabel = QLabel(str(item['price']), self)
-            imageValueLabel = QLabel(str(item['image']), self)
-            buttonRemove = QPushButton('-', self)
-
-            if (index % 2 == 0):
-                idValueLabel.setStyleSheet("background: gray")
-                nameValueLabel.setStyleSheet("background: gray")
-                priceValueLabel.setStyleSheet("background: gray")
-                imageValueLabel.setStyleSheet("background: gray")
-
-            self.gridLayout.addWidget(idValueLabel, index, 0)
-            self.gridLayout.addWidget(nameValueLabel, index, 1)
-            self.gridLayout.addWidget(priceValueLabel, index, 2)
-            self.gridLayout.addWidget(imageValueLabel, index, 3)
-            self.gridLayout.addWidget(buttonRemove, index, 4)
+            position = self.products.index(item) + 1
+            self.addNewProduct(str(item['id']), str(item['name']), str(item['price']), str(item['image']), position)
 
         self.newID = QLineEdit()
         self.newName = QLineEdit()
@@ -71,9 +54,42 @@ class MyWindow(QWidget):
         self.gridLayout.addWidget(self.buttonAdd, len(self.products) + 2, 4)
         self.show()
 
+    def addNewProduct(self, id, name, price, image, position):
+        idValueLabel = QLabel(id, self)
+        nameValueLabel = QLabel(name, self)
+        priceValueLabel = QLabel(price, self)
+        imageValueLabel = QLabel(image, self)
+        buttonRemove = QPushButton('-', self)
+
+        if (position % 2 == 0):
+            idValueLabel.setStyleSheet("background: gray")
+            nameValueLabel.setStyleSheet("background: gray")
+            priceValueLabel.setStyleSheet("background: gray")
+            imageValueLabel.setStyleSheet("background: gray")
+
+        self.gridLayout.addWidget(idValueLabel, position, 0)
+        self.gridLayout.addWidget(nameValueLabel, position, 1)
+        self.gridLayout.addWidget(priceValueLabel, position, 2)
+        self.gridLayout.addWidget(imageValueLabel, position, 3)
+        self.gridLayout.addWidget(buttonRemove, position, 4)
+
     def addProduct(self):
         self.reader.addSingleProduct(self.newID.text(), self.newName.text(), self.newPrice.text(), self.newImage.text())
-        self.initUI()
+        self.products = self.reader.readProducts()
+        self.addNewProduct(self.newID.text(), self.newName.text(), self.newPrice.text(), self.newImage.text(),
+                           len(self.products))
+        self.gridLayout.removeWidget(self.newID)
+        self.gridLayout.removeWidget(self.newName)
+        self.gridLayout.removeWidget(self.newPrice)
+        self.gridLayout.removeWidget(self.newImage)
+        self.gridLayout.removeWidget(self.buttonAdd)
+
+        self.gridLayout.addWidget(self.newID, len(self.products) + 2, 0)
+        self.gridLayout.addWidget(self.newName, len(self.products) + 2, 1)
+        self.gridLayout.addWidget(self.newPrice, len(self.products) + 2, 2)
+        self.gridLayout.addWidget(self.newImage, len(self.products) + 2, 3)
+        self.gridLayout.addWidget(self.buttonAdd, len(self.products) + 2, 4)
+
 
 def main():
     app = QApplication(sys.argv)
