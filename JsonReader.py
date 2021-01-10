172da4feedc73b23bbbeeec44afde46f65cfd87e
DESKTOP-HJLQUDI\EasyK <tomasz.milewski.194@gmail.com>
2021-01-10 16:09:30 +0100

    json reader with update list method (required to save checked products)

--- JsonReader.py
+++ JsonReader.py
@@ -23,18 +23,15 @@ class JsonReader:
         self.saveProducts(products)
 
     def saveLists(self, lists):
-        print('save lists')
         with open("lists.json", "w") as json_file:
             json.dump(lists, json_file, indent=2)
 
     def readLists(self):
-        print('read lists')
         with open("lists.json") as json_data_file:
             data = json.load(json_data_file)
             return data
 
     def addSingleList(self, listName, products):
-        print('add single list')
         list = {
             "list_name": listName,
              "products": products
@@ -42,4 +39,12 @@ class JsonReader:
 
         lists = self.readLists()
         lists.append(list)
+        self.saveLists(lists)
+
+    def updateList(self, listName, list):
+        lists = self.readLists().copy()
+        for singleList in lists:
+            if singleList["list_name"] == listName:
+                singleList["products"] = list["products"]
+
         self.saveLists(lists)
